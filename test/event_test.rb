72016e9a0cb800c2e52c62dee766ffd67f503975
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-15 07:51:40 -1000

    add test for total_inventory

--- test/event_test.rb
+++ test/event_test.rb
@@ -141,8 +141,10 @@ class EventTest < MiniTest::Test
     food_truck3.stock(item1, 65)
     food_truck3.stock(item3, 10)
     event.add_food_truck(food_truck1)
+    assert_equal ({item1=>{quantity: 35, food_trucks: [food_truck1]},item2=>{quantity:7, food_trucks:[food_truck1]}}), event.total_inventory
     event.add_food_truck(food_truck2)
     event.add_food_truck(food_truck3)
     assert_equal ({item1=>{quantity: 100, food_trucks: [food_truck1,food_truck3]}, item2 =>{quantity:7, food_trucks:[food_truck1]}, item3=>{quantity:35, food_trucks:[food_truck2, food_truck3]}}), event.total_inventory
   end
+
 end
